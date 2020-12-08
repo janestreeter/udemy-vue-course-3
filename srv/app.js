76008151c49198f5f3eb50469ea929d96beb7de9
Jane Streeter <jane.america.streeter@gmail.com>
2020-12-07 22:32:06 -0600

    started lesson 20

--- srv/app.js
+++ srv/app.js
@@ -1,19 +1,9 @@
-Vue.createApp({
-  data: () => ({
-    name: "jane",
-    age: 11,
-    randimal:
-      "https://images.theconversation.com/files/337593/original/file-20200526-106811-ql6d51.jpg",
-  }),
-  methods: {
-    penta() {
-      return this.age + 5;
-    },
-    randy() {
-      return Math.random();
-    },
-    test(evt) {
-      this.name = evt.target.value;
-    },
+const app = Vue.createApp({
+  data() {
+    return {
+      counter: 0,
+    };
   },
-}).mount("#assignment");
+});
+
+app.mount('#events');
