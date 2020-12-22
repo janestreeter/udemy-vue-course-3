eb628362682ee4d34a3ea2940bcb8fccebcd3485
Jane Streeter <jane.america.streeter@gmail.com>
2020-12-22 15:13:48 -0600

    im moving on

--- srv/app.js
+++ srv/app.js
@@ -2,12 +2,19 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
-      name: ''
+      name: "",
     };
   },
   methods: {
-    setName(event, lastName) {
-      this.name = event.target.value + ' ' + lastName;
+    outputFullname() {
+      console.log("Running again...");
+      if (this.name === "") {
+        return "";
+      }
+      return this.name + " " + "Streeter";
+    },
+    setName(event) {
+      this.name = event.target.value;
     },
     add(num) {
       this.counter = this.counter + num;
@@ -15,8 +22,11 @@ const app = Vue.createApp({
     reduce(num) {
       this.counter = this.counter - num;
       // this.counter--;
-    }
-  }
+    },
+    resetInput() {
+      this.name = "";
+    },
+  },
 });
 
-app.mount('#events');
+app.mount("#events");
