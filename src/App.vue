9a61e73e42db9fe66d325943959353bceb6ce545
Jane Streeter <jane.america.streeter@gmail.com>
2021-01-30 14:50:55 -0600

    moving on to section 11

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <the-header title="RememberMe"></the-header>
+  <the-resources></the-resources>
+</template>
+
+<script>
+import TheHeader from "./components/layouts/TheHeader.vue";
+import TheResources from "./components/learning-resources/TheResource.vue";
+
+export default {
+  components: {
+    TheHeader,
+    TheResources,
+  },
+};
+</script>
+
+<style></style>
